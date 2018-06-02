0286eeb3fa8853f4197e4407b968beec3911ad5a
mgoworko <michal-goworko@outlook.com>
2018-06-02 13:47:48 +0200

    Password change backend

--- src/main/java/edu/pw/hwmonitor/controllers/CommonController.java
+++ src/main/java/edu/pw/hwmonitor/controllers/CommonController.java
@@ -2,10 +2,12 @@ package edu.pw.hwmonitor.controllers;
 
 import edu.pw.hwmonitor.controllers.requests.PasswordUpdateRequest;
 import edu.pw.hwmonitor.security.SecurityManager;
+import edu.pw.hwmonitor.user.User;
 import edu.pw.hwmonitor.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,9 +26,14 @@ public class CommonController {
     @PostMapping("/common/update-password")
     public ResponseEntity<HttpStatus> updatePassword(@RequestBody PasswordUpdateRequest passwordUpdateRequest) {
         String username = securityManager.getUsername();
+        User user = userRepository.findByUsernameEquals(username).get();
 
-        // TODO update password
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        if(!bCryptPasswordEncoder.matches(passwordUpdateRequest.getOldPassword(),user.getPassword())) return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
 
-        return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        user.setPassword(passwordUpdateRequest.getNewPassword());
+        userRepository.save(user);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
