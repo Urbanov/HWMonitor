158d1ba223a4a91a8c7b38ba22283c4a08fba68a
Mikolaj Umanski <mik.umanski@gmail.com>
2018-06-03 13:05:17 +0200

    Added admin controller tests

---
+++ src/test/java/edu/pw/hwmonitor/AdminControllerTest.java
@@ -0,0 +1,91 @@
+package edu.pw.hwmonitor;
+
+import edu.pw.hwmonitor.companies.Company;
+import edu.pw.hwmonitor.companies.CompanyRepository;
+import edu.pw.hwmonitor.companies.role.RoleRepository;
+import edu.pw.hwmonitor.controllers.AdminController;
+import edu.pw.hwmonitor.user.User;
+import edu.pw.hwmonitor.user.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.mock;
+import static org.powermock.api.mockito.PowerMockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Main.class)
+public class AdminControllerTest {
+
+    private MockMvc mockMvc;
+
+    private CompanyRepository companyRepository;
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private AdminController controller;
+
+    @Before
+    public void setUp() throws Exception {
+        companyRepository = Mockito.mock(CompanyRepository.class);
+        userRepository = Mockito.mock(UserRepository.class);
+        roleRepository = Mockito.mock(RoleRepository.class);
+        controller = new AdminController(companyRepository, userRepository, roleRepository);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void creatingCompanyWithNewNameShouldReturnOk() throws Exception {
+
+        //when
+        mockMvc.perform(post("/admin/create-company")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"test\", \"username\":\"test\", \"password\":\"test\"}")
+                .accept(MediaType.APPLICATION_JSON))
+
+        //then
+        .andExpect(status().isOk());
+    }
+
+    @Test
+    public void creatingCompanyWithAlreadyExistingCompanyNameShouldReturnNotAcceptable() throws Exception {
+
+        //given
+        Company testCompany = mock(Company.class);
+
+        //when
+        when(companyRepository.findByNameEquals("test")).thenReturn(Optional.of(testCompany));
+        mockMvc.perform(post("/admin/create-company")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"test\", \"username\":\"test\", \"password\":\"test\"}")
+                .accept(MediaType.APPLICATION_JSON))
+
+        //then
+        .andExpect(status().isNotAcceptable());
+    }
+
+    @Test
+    public void creatingCompanyWithExistingUserNameShouldReturnNotAcceptable() throws Exception {
+        //given
+        User testUser = mock(User.class);
+
+        //when
+        when(userRepository.findByUsernameEquals("test")).thenReturn(Optional.of(testUser));
+        mockMvc.perform(post("/admin/create-company")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"test\", \"username\":\"test\", \"password\":\"test\"}")
+                .accept(MediaType.APPLICATION_JSON))
+
+        //then
+        .andExpect(status().isNotAcceptable());
+    }
+}
